50245cb60f1244c9b862534e7db255c87650b05a
lina-3-rgb <polinkavorobeva3@gmail.com>
2025-10-22 14:15:14 +0300

    updates

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,21 @@
+﻿
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double x;
+	cout << "Введите х";
+	cin >> x;
+
+	double y;
+	cout << "Введиде у";
+	cin >> y;
+
+	double c = pow(y * pow(x, 2) - 1, 2) / (2) * (pow(cos(y), 2)) * y - sin(pow(x, 2));
+	cout << "Ответ" << c;
+}
+
