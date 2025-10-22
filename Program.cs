b697f2e3012bb4f5d4368f67e87f2de8a7c1a4e7
lina-3-rgb <polinkavorobeva3@gmail.com>
2025-10-22 14:48:53 +0300

    c#

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace di
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите x:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double s = Math.Pow((y * Math.Pow(x, 2) - 1), 2) / (2) * (Math.Pow(Math.Cos(y), 2) - Math.Sin(Math.Pow(x,2)));
+            Console.WriteLine("Ответ:" + s);
+        }
+    }
+}
